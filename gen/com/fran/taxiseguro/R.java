26c4fde1d1f3d2e5b7b96eea197654fb48aba612
Fran <franjmartin21@gmail.com>
2013-10-27 11:56:48 -0500

    Support for GMS messaging

--- gen/com/fran/taxiseguro/R.java
+++ gen/com/fran/taxiseguro/R.java
@@ -12,29 +12,41 @@ public final class R {
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
-
-         Customize dimensions originally defined in res/values/dimens.xml (such as
-         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
-    
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int btn_default_disabled_focused_holo_light=0x7f020000;
+        public static final int btn_default_disabled_holo_light=0x7f020001;
+        public static final int btn_default_focused_holo_light=0x7f020002;
+        public static final int btn_default_holo_light=0x7f020003;
+        public static final int btn_default_normal_holo_light=0x7f020004;
+        public static final int btn_default_pressed_holo_light=0x7f020005;
+        public static final int edit_text_holo_light=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int textfield_activated_holo_light=0x7f020008;
+        public static final int textfield_default_holo_light=0x7f020009;
+        public static final int textfield_disabled_focused_holo_light=0x7f02000a;
+        public static final int textfield_disabled_holo_light=0x7f02000b;
+        public static final int textfield_focused_holo_light=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int comenzarButton=0x7f080002;
+        public static final int action_settings=0x7f080005;
+        public static final int comenzarButton=0x7f080003;
+        public static final int mapResultView=0x7f080002;
         public static final int matriculaLabel=0x7f080000;
         public static final int matriculaText=0x7f080001;
+        public static final int tvBroadcastMessage=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_taxiplate=0x7f030001;
+        public static final int activity_restloader=0x7f030001;
+        public static final int activity_taxiplate=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int restloader=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -43,33 +55,12 @@ public final class R {
         /**  STRINGS PARA TaxiPlateActivity 
          */
         public static final int plate_label=0x7f050003;
+        public static final int title_activity_restloader=0x7f050004;
     }
     public static final class style {
-        /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
-         */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f060001;
+        public static final int ButtontaxiTheme=0x7f060001;
+        public static final int EditTexttaxiTheme=0x7f060000;
+        public static final int ImageButtontaxiTheme=0x7f060002;
+        public static final int taxiTheme=0x7f060003;
     }
 }
--- src/com/fran/taxiseguro/TaxiPlateActivity.java
+++ src/com/fran/taxiseguro/TaxiPlateActivity.java
@@ -1,5 +1,7 @@
 package com.fran.taxiseguro;
 
+import java.util.UUID;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -7,10 +9,17 @@ import org.json.JSONTokener;
 import android.app.Activity;
 import android.app.LoaderManager;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.Loader;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -20,14 +29,31 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fran.taxiseguro.loader.RESTLoader;
+import com.google.android.gcm.GCMRegistrar;
 
 public class TaxiPlateActivity extends Activity implements LoaderCallbacks<RESTLoader.RESTResponse>{
 	
 	private static final String TAG = TaxiPlateActivity.class.getName();
 	
+	// This is the project id generated from the Google console when
+	// you defined a Google APIs project.
+	private static final String PROJECT_ID = "647262737049";
+	
+	// This string will hold the lengthy registration id that comes
+	// from GCMRegistrar.register()
+	private String regId = "";
+	
+	// This intent filter will be set to filter on the string "GCM_RECEIVED_ACTION"
+	IntentFilter gcmFilter;
+
+	private String broadcastMessage = "No broadcast message";
+
+	
 	private EditText matriculaText;
 	private Button comenzarButton;
 	private TextView resultView;
+	private TextView tvBroadcastMessage;
+	ProgressDialog dialog;
 	
 	private static final String ARGS_URI    = "com.fran.taxiseguro.ARGS_URI";
 	private static final String ARGS_PARAMS = "com.fran.taxiseguro.ARGS_PARAMS";
@@ -45,7 +71,10 @@ public class TaxiPlateActivity extends Activity implements LoaderCallbacks<RESTL
 		comenzarButton = (Button)findViewById(R.id.comenzarButton);
 		matriculaText = (EditText)findViewById(R.id.matriculaText);
 		resultView = (TextView)findViewById(R.id.mapResultView);
-		
+		tvBroadcastMessage = (TextView)findViewById(R.id.tvBroadcastMessage);
+		/**
+		 * When clicking the button, it will call the RestService
+		 */
 		comenzarButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -55,16 +84,14 @@ public class TaxiPlateActivity extends Activity implements LoaderCallbacks<RESTL
 			}
 		});
 		
-		LoaderManager lm = getLoaderManager();
-		// ****************************************
-		Uri gMapsSearchUri = Uri.parse("http://maps.googleapis.com/maps/api/geocode/json");
-	     Bundle params = new Bundle();
-	     Bundle args = new Bundle();
-	     args.putParcelable(ARGS_URI, gMapsSearchUri);
-	     args.putParcelable(ARGS_PARAMS, params);
-	     // Initialize the Loader.
-	     //getSupportLoaderManager().initLoader(LOADER_MAP_SEARCH, args, this);
-	     lm.initLoader(LOADER_MAP_SEARCH, args, this);
+		// Create our IntentFilter, which will be used in conjunction with a
+		// broadcast receiver.
+		gcmFilter = new IntentFilter();
+		gcmFilter.addAction("GCM_RECEIVED_ACTION");
+
+		registerClient();
+		Log.i("REG",getDeviceID(this));
+		
 	}
 	
 	private String validatePlate(){
@@ -77,6 +104,10 @@ public class TaxiPlateActivity extends Activity implements LoaderCallbacks<RESTL
 		return matriculaText.getText().toString();
 	}
 	
+	/**
+	 * TO SEND MESSAGES THROUGH WHATSAPP!!
+	 * @param message
+	 */
 	public void onClickWhatsApp(String message) {
 
 	    Intent waIntent = new Intent(Intent.ACTION_SEND);
@@ -92,49 +123,60 @@ public class TaxiPlateActivity extends Activity implements LoaderCallbacks<RESTL
 	    }
 	}
 	
+	/**
+	 * **************************************************************************
+	 */
+	
+	/**
+	 * LOADERS THAT CONNECT TO THE RESTSERVICE AND RETRIEVE THE DATA FROM THE SERVER
+	 */
+	
 	private void getRestData(){
-		// This is our REST action.
-	     Uri gMapsSearchUri = Uri.parse("http://maps.googleapis.com/maps/api/geocode/json");
+		 dialog = ProgressDialog.show(this, "Searching", "Getting data from server");
+		 LoaderManager lm = getLoaderManager();
+		 Uri gMapsSearchUri = Uri.parse("http://maps.googleapis.com/maps/api/geocode/json");
 	     Bundle params = new Bundle();
+	     params.putBoolean("sensor", false);
+	     params.putString("address", matriculaText.getText().toString());
 	     Bundle args = new Bundle();
 	     args.putParcelable(ARGS_URI, gMapsSearchUri);
 	     args.putParcelable(ARGS_PARAMS, params);
 	     // Initialize the Loader.
 	     //getSupportLoaderManager().initLoader(LOADER_MAP_SEARCH, args, this);
+	     lm.initLoader(LOADER_MAP_SEARCH, args, this);
 	}
 	
-	
-	 @Override
-	 public Loader<RESTLoader.RESTResponse> onCreateLoader(int id, Bundle args) {
-	     if (args != null && args.containsKey(ARGS_URI) && args.containsKey(ARGS_PARAMS)) {
-	         Uri    action = args.getParcelable(ARGS_URI);
-	         Bundle params = args.getParcelable(ARGS_PARAMS);
+	@Override
+	public Loader<RESTLoader.RESTResponse> onCreateLoader(int id, Bundle args) {
+		if (args != null && args.containsKey(ARGS_URI) && args.containsKey(ARGS_PARAMS)) {
+			Uri    action = args.getParcelable(ARGS_URI);
+			Bundle params = args.getParcelable(ARGS_PARAMS);
+			return new RESTLoader(this, RESTLoader.HTTPVerb.GET, action, params);
+		}
+		return null;
+	}
+ 
+	@Override
+	public void onLoadFinished(Loader<RESTLoader.RESTResponse> loader, RESTLoader.RESTResponse data) {
+	     int    code = data.getCode();
+	     String json = data.getData();
+	     
+	     // Check to see if we got an HTTP 200 code and have some data.
+	     if (code == 200 && !json.equals("")) {
 	         
-	         return new RESTLoader(this, RESTLoader.HTTPVerb.GET, action, params);
+	         // For really complicated JSON decoding I usually do my heavy lifting
+	         // Gson and proper model classes, but for now let's keep it simple
+	         // and use a utility method that relies on some of the built in
+	         // JSON utilities on Android.
+	         String resultMap = getJson(json);
+	         
+	         resultView.setText(resultMap);
 	     }
-	     return null;
-	 }
-
- @Override
- public void onLoadFinished(Loader<RESTLoader.RESTResponse> loader, RESTLoader.RESTResponse data) {
-     int    code = data.getCode();
-     String json = data.getData();
-     
-     // Check to see if we got an HTTP 200 code and have some data.
-     if (code == 200 && !json.equals("")) {
-         
-         // For really complicated JSON decoding I usually do my heavy lifting
-         // Gson and proper model classes, but for now let's keep it simple
-         // and use a utility method that relies on some of the built in
-         // JSON utilities on Android.
-         String resultMap = getJson(json);
-         
-         resultView.setText(resultMap);
-     }
-     else{
-    	 Toast.makeText(this, "Failed to load map data. Check your internet settings.", Toast.LENGTH_SHORT).show();
-     }
- }
+	     else{
+	    	 Toast.makeText(this, "Failed to load map data. Check your internet settings.", Toast.LENGTH_SHORT).show();
+	     }
+	     handler.sendEmptyMessage(0);
+	}
 
 	 @Override
 	 public void onLoaderReset(Loader<RESTLoader.RESTResponse> loader) {
@@ -152,7 +194,165 @@ public class TaxiPlateActivity extends Activity implements LoaderCallbacks<RESTL
 		return mapJson.toString();
 	 }
 	
+	 private Handler handler = new Handler() {
+	        public void handleMessage(Message msg) {
+	            dialog.dismiss();
+	        }
+	 };
+
+		
+
+
+		
+
+	// This broadcastreceiver instance will receive messages broadcast
+	// with the action "GCM_RECEIVED_ACTION" via the gcmFilter
+	
+	// A BroadcastReceiver must override the onReceive() event.
+	private BroadcastReceiver gcmReceiver = new BroadcastReceiver() {
+	
+		@Override
+		public void onReceive(Context context, Intent intent) {
+	
+			broadcastMessage = intent.getExtras().getString("gcm");
+	
+			if (broadcastMessage != null) {
+				// display our received message
+				tvBroadcastMessage.setText(broadcastMessage);
+			}
+		}
+	};
+		
+	// This registerClient() method checks the current device, checks the
+	// manifest for the appropriate rights, and then retrieves a registration id
+	// from the GCM cloud.  If there is no registration id, GCMRegistrar will
+	// register this device for the specified project, which will return a
+	// registration id.
+	public void registerClient() {
+	
+		try {
+			// Check that the device supports GCM (should be in a try / catch)
+			GCMRegistrar.checkDevice(this);
+	
+			// Check the manifest to be sure this app has all the required
+			// permissions.
+			GCMRegistrar.checkManifest(this);
+	
+			// Get the existing registration id, if it exists.
+			regId = GCMRegistrar.getRegistrationId(this);
+	
+			if (regId.equals("")) {
+	
+				Toast.makeText(this, "Registering...", Toast.LENGTH_SHORT);
+	
+				// register this device for this project
+				GCMRegistrar.register(this, PROJECT_ID);
+				regId = GCMRegistrar.getRegistrationId(this);
+	
+				Toast.makeText(this, "Registered", Toast.LENGTH_LONG);
+	
+				// This is actually a dummy function.  At this point, one
+				// would send the registration id, and other identifying
+				// information to your server, which should save the id
+				// for use when broadcasting messages.
+				sendRegistrationToServer();
+			} else {
+				Toast.makeText(this, "Already registered", Toast.LENGTH_LONG);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+		}
+		// This is part of our CHEAT.  For this demo, you'll need to
+		// capture this registration id so it can be used in our demo web
+		// service.
+		Log.d(TAG, regId);
+	}
+	
+
+	private void sendRegistrationToServer() {
+		// This is an empty placeholder for an asynchronous task to post the
+		// registration
+		// id and any other identifying information to your server.
+	}
+
+	// If the user changes the orientation of his phone, the current activity
+	// is destroyed, and then re-created.  This means that our broadcast message
+	// will get wiped out during re-orientation.
+	// So, we save the broadcastmessage during an onSaveInstanceState()
+	// event, which is called prior to the destruction of the activity.
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+
+		super.onSaveInstanceState(savedInstanceState);
+
+		savedInstanceState.putString("BroadcastMessage", broadcastMessage);
+
+	}
+
+	// When an activity is re-created, the os generates an onRestoreInstanceState()
+	// event, passing it a bundle that contains any values that you may have put
+	// in during onSaveInstanceState()
+	// We can use this mechanism to re-display our last broadcast message.
+	
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState) {
+		
+		super.onRestoreInstanceState(savedInstanceState);
+
+		broadcastMessage = savedInstanceState.getString("BroadcastMessage");
+		tvBroadcastMessage.setText(broadcastMessage);
+
+	}
+
+	// If our activity is paused, it is important to UN-register any
+	// broadcast receivers.
+	@Override
+	protected void onPause() {
+		
+		unregisterReceiver(gcmReceiver);
+		super.onPause();
+	}
+	
+	// When an activity is resumed, be sure to register any
+	// broadcast receivers with the appropriate intent
+	@Override
+	protected void onResume() {
+		super.onResume();
+		registerReceiver(gcmReceiver, gcmFilter);
+
+	}
+
+
+	// NOTE the call to GCMRegistrar.onDestroy()
+	@Override
+	public void onDestroy() {
+
+		GCMRegistrar.onDestroy(this);
+
+		super.onDestroy();
+	}
 	
 	
-	
+	/**
+	 * TEST TO GET THE REGISTRY ID
+	 */
+	public static String getDeviceID(Context context) {
+	    final TelephonyManager tm = (TelephonyManager) context
+	            .getSystemService(Context.TELEPHONY_SERVICE);
+
+	    final String tmDevice, tmSerial, tmPhone, androidId;
+	    tmDevice = "" + tm.getDeviceId();
+	    tmSerial = "";// + tm.getSimSerialNumber();
+	    androidId = ""
+	            + android.provider.Settings.Secure.getString(
+	                    context.getContentResolver(),
+	                    android.provider.Settings.Secure.ANDROID_ID);
+
+	    UUID deviceUuid = new UUID(androidId.hashCode(),
+	            ((long) tmDevice.hashCode() << 32) | tmSerial.hashCode());
+	    String deviceId = deviceUuid.toString();
+
+	    return deviceId;
+	}
 }
